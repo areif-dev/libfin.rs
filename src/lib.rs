de93e4bd2dac5ddbbb10324667f4db51197b460e
areif-dev <ajreifsnyder@protonmail.com>
2024-01-19 13:17:27 -0500

    Convert calculate_rsi, ema, macd to use standard rust datatypes instead of ndarray

--- src/lib.rs
+++ src/lib.rs
@@ -22,38 +22,40 @@ impl std::error::Error for IndicatorError {}
 ///
 /// # Arguments
 ///
-/// * `prices` - An array of price data.
+/// * `prices` - A slice of price data.
 /// * `window` - The size of the window for calculating RSI.
 ///
 /// # Returns
 ///
-/// A Result containing an array of RSI values or an `IndicatorError` if there is not enough data.
+/// A Result containing a vector of RSI values or an `IndicatorError` if there is not enough data.
 ///
 /// # Errors
 ///
 /// Returns an `IndicatorError::NotEnoughData` if the length of `prices` is less than or equal to
 /// `window`.
-pub fn calculate_rsi(
-    prices: ArrayBase<ViewRepr<&f64>, Ix1>,
-    window: usize,
-) -> Result<ArrayBase<OwnedRepr<f64>, Ix1>, IndicatorError> {
+pub fn calculate_rsi(prices: &[f64], window: usize) -> Result<Vec<f64>, IndicatorError> {
     // Check if prices array has enough elements
     if prices.len() <= window {
         return Err(IndicatorError::NotEnoughData(
-            "Not enough data points to caluclate RSI".to_string(),
+            "Not enough data points to calculate RSI".to_string(),
         ));
     }
 
     // Calculate price changes
-    let price_changes = prices.slice(s![1..]).to_owned() - prices.slice(s![..-1]);
+    let price_changes = prices[1..].iter().zip(prices.iter()).map(|(x, y)| x - y);
 
     // Separate gains and losses
-    let gains = price_changes.mapv(|x| if x > 0.0 { x } else { 0.0 });
-    let losses = price_changes.mapv(|x| if x < 0.0 { -x } else { 0.0 });
+    let gains: Vec<f64> = price_changes
+        .clone()
+        .map(|x| if x > 0.0 { x } else { 0.0 })
+        .collect();
+    let losses: Vec<f64> = price_changes
+        .map(|x| if x < 0.0 { -x } else { 0.0 })
+        .collect();
 
     // Calculate average gains and losses over the window
-    let mut avg_gain = gains.slice(s![..window]).sum() / window as f64;
-    let mut avg_loss = losses.slice(s![..window]).sum() / window as f64;
+    let mut avg_gain = gains.iter().take(window).sum::<f64>() / window as f64;
+    let mut avg_loss = losses.iter().take(window).sum::<f64>() / window as f64;
 
     // Calculate RSI for each element in the specified window to the end
     let mut rsi_values = Vec::with_capacity(prices.len() - window);
@@ -76,28 +78,24 @@ pub fn calculate_rsi(
         rsi_values.push(rsi);
     }
 
-    // Convert the Vec<f64> to an ArrayBase
-    Ok(arr1(&rsi_values))
+    Ok(rsi_values)
 }
 
 /// Calculates the Exponential Moving Average (EMA) for a given price array and window size.
 ///
 /// # Arguments
 ///
-/// * `prices` - An array of price data.
+/// * `prices` - A slice of price data.
 /// * `window` - The size of the window for calculating EMA.
 ///
 /// # Returns
 ///
-/// A Result containing an array of EMA values or an `IndicatorError` if there is not enough data.
+/// A Result containing a vector of EMA values or an `IndicatorError` if there is not enough data.
 ///
 /// # Errors
 ///
 /// Returns an `IndicatorError::NotEnoughData` if the length of `prices` is less than `window`.
-pub fn calculate_ema(
-    prices: ArrayBase<ViewRepr<&f64>, Ix1>,
-    window: usize,
-) -> Result<ArrayBase<OwnedRepr<f64>, Ix1>, IndicatorError> {
+pub fn calculate_ema(prices: &[f64], window: usize) -> Result<Vec<f64>, IndicatorError> {
     if prices.len() < window {
         return Err(IndicatorError::NotEnoughData(
             "`prices` must have at least `window` items".to_string(),
@@ -106,7 +104,7 @@ pub fn calculate_ema(
 
     let smoothing = 2.0 / (window as f64 + 1.0);
 
-    let sma = prices.slice(s![..window]).sum() / window as f64;
+    let sma = prices.iter().take(window).sum::<f64>() / window as f64;
     let mut ema_values = Vec::with_capacity(prices.len() - window);
     ema_values.push(sma);
 
@@ -118,14 +116,14 @@ pub fn calculate_ema(
         ema_values.push(ema);
     }
 
-    Ok(arr1(&ema_values))
+    Ok(ema_values)
 }
 
 /// Calculates the Moving Average Convergence Divergence (MACD) for a given price array and parameters.
 ///
 /// # Arguments
 ///
-/// * `prices` - An array of price data.
+/// * `prices` - A slice of price data.
 /// * `short_window` - The size of the short-term EMA window.
 /// * `long_window` - The size of the long-term EMA window.
 /// * `signal_window` - The size of the signal line window.
@@ -139,29 +137,29 @@ pub fn calculate_ema(
 /// Returns an `IndicatorError::NotEnoughData` if the length of `prices` is insufficient to
 /// calculate any of the moving averages for the `short_window`, `long_window`, or the `signal_window`.
 pub fn calculate_macd(
-    prices: ArrayBase<ViewRepr<&f64>, Ix1>,
+    prices: &[f64],
     short_window: usize,
     long_window: usize,
     signal_window: usize,
-) -> Result<
-    (
-        ArrayBase<OwnedRepr<f64>, Ix1>,
-        ArrayBase<OwnedRepr<f64>, Ix1>,
-        ArrayBase<OwnedRepr<f64>, Ix1>,
-    ),
-    IndicatorError,
-> {
+) -> Result<(Vec<f64>, Vec<f64>, Vec<f64>), IndicatorError> {
     let mut ema_short = calculate_ema(prices, short_window)?;
     let ema_long = calculate_ema(prices, long_window)?;
-    ema_short = ema_short.slice(s![long_window - short_window..]).to_owned();
+    ema_short = ema_short[long_window - short_window..].to_owned();
 
-    let mut macd_line = ema_short - ema_long;
-    let signal_line = calculate_ema(macd_line.slice(s![..]), signal_window)?;
-    macd_line = macd_line
-        .slice(s![macd_line.len() - signal_line.len()..])
-        .to_owned();
+    let mut macd_line = ema_short
+        .iter()
+        .zip(&ema_long)
+        .map(|(a, b)| a - b)
+        .collect::<Vec<f64>>();
+    let signal_line = calculate_ema(&macd_line, signal_window)?;
+    macd_line = macd_line[macd_line.len() - signal_line.len()..].to_owned();
 
-    let histogram = macd_line.clone() - signal_line.clone();
+    let histogram = macd_line
+        .clone()
+        .iter()
+        .zip(&signal_line)
+        .map(|(a, b)| a - b)
+        .collect::<Vec<f64>>();
     Ok((macd_line, signal_line, histogram))
 }
 
@@ -173,15 +171,15 @@ mod tests {
     #[test]
     fn test_calculate_rsi() {
         // Test case with enough data
-        let prices = array![1.0, 2.0, 3.0, 4.0, 5.0];
+        let prices = vec![1.0, 2.0, 3.0, 4.0, 5.0];
         let window = 3;
-        let result = calculate_rsi(prices.view(), window).unwrap();
-        assert_eq!(result, array![100.0, 100.0]);
+        let result = calculate_rsi(prices.as_slice(), window).unwrap();
+        assert_eq!(result, vec![100.0, 100.0]);
 
         // Test case with not enough data
-        let prices = array![1.0, 2.0];
+        let prices = vec![1.0, 2.0];
         let window = 3;
-        let result = calculate_rsi(prices.view(), window);
+        let result = calculate_rsi(prices.as_slice(), window);
         assert!(result.is_err());
         match result {
             Err(IndicatorError::NotEnoughData(_)) => (),
@@ -194,15 +192,15 @@ mod tests {
     #[test]
     fn test_calculate_ema() {
         // Test case with enough data
-        let prices = array![1.0, 2.0, 3.0, 4.0, 5.0];
+        let prices = vec![1.0, 2.0, 3.0, 4.0, 5.0];
         let window = 3;
-        let result = calculate_ema(prices.view(), window).unwrap();
-        assert_eq!(result, array![2.0, 3.0, 4.0]);
+        let result = calculate_ema(prices.as_slice(), window).unwrap();
+        assert_eq!(result, vec![2.0, 3.0, 4.0]);
 
         // Test case with not enough data
-        let prices = array![1.0, 2.0];
+        let prices = vec![1.0, 2.0];
         let window = 3;
-        let result = calculate_ema(prices.view(), window);
+        let result = calculate_ema(prices.as_slice(), window);
         assert!(result.is_err());
         match result {
             Err(IndicatorError::NotEnoughData(_)) => (),
@@ -215,20 +213,20 @@ mod tests {
     #[test]
     fn test_calculate_macd() {
         // Test case with enough data
-        let prices = array![1.0, 2.0, 3.0, 4.0, 5.0];
+        let prices = vec![1.0, 2.0, 3.0, 4.0, 5.0];
         let short_window = 2;
         let long_window = 4;
         let signal_window = 2;
         let result =
-            calculate_macd(prices.view(), short_window, long_window, signal_window).unwrap();
-        assert_eq!(result, (array![1.0], array![1.0], array![0.0]));
+            calculate_macd(prices.as_slice(), short_window, long_window, signal_window).unwrap();
+        assert_eq!(result, (vec![1.0], vec![1.0], vec![0.0]));
 
         // Test case with not enough data
-        let prices = array![1.0, 2.0];
+        let prices = vec![1.0, 2.0];
         let short_window = 2;
         let long_window = 4;
         let signal_window = 2;
-        let result = calculate_macd(prices.view(), short_window, long_window, signal_window);
+        let result = calculate_macd(prices.as_slice(), short_window, long_window, signal_window);
         assert!(result.is_err());
         match result {
             Err(IndicatorError::NotEnoughData(_)) => (),
